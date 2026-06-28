2489ce271a93a916bc9517de6b36e1a622dfa699
Shubh <rathodshubham7711@gmail.com>
2026-06-28 15:38:15 +0530

    Alias Learned

---
+++ Alias_Practice/alias_practice.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE ALIAS_PRACTICE;
+USE ALIAS_PRACTICE;
+
+-- In This Chapter We'll discuss about alias in SQL
+
+-- ALIAS -- An alias is created with the *AS* keyword and is often used to make column name readable,
+
+-- Let's see the Syntax:
+SELECT Column_Nams AS Alias_name
+FROM table_name;
+
+-- Let's See the Example
+
+SELECT StudentName AS NAME 
+FROM STUDENT;
